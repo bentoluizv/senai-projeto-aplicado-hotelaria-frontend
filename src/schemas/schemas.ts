ed28ad3a60ae4a1ad4e296fc7a9e4e20258d8fc2
bentoluizv <bentoluizv@gmail.com>
2024-06-15 08:39:45 -0300

    added type coercion for integer data that has string input

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { isValidCpf } from "../utils/isValidCpf";
 
 export const guestSchema = z.object({
-    created_at: z.string().datetime("Data Inválida").optional(),
+    created_at: z.string().optional(),
     document: z.string().refine((value) => isValidCpf(value), "Cpf Inválido"),
     name: z.string().trim().min(1, "Você deve preencher esse campo"),
     surname: z.string().trim().min(1, "Você deve preencher esse campo"),
@@ -11,24 +11,24 @@ export const guestSchema = z.object({
 });
 
 export const accommodationSchema = z.object({
-    id : z.number().optional(),
+    id : z.coerce.number().optional(),
     created_at: z.string().optional(),
     name: z.string().trim().min(1, "Você deve preencher esse campo"),
     status: z.string().optional(),
-    total_guests: z.number().min(1, "Total de hóspedes não pode  ser menor que um"),
-    single_beds: z.number().min(0, "Quantidade inválida"),
-    double_beds: z.number().min(0, "Quantidade inválida"),
-    min_nights: z.number().min(0, "Quantidade inválida"),
-    price: z.number().min(0, "Preço Inválido").transform((price) => Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(price)),
+    total_guests: z.coerce.number().min(1, "Total de hóspedes não pode  ser menor que um"),
+    single_beds: z.coerce.number().min(0, "Quantidade inválida"),
+    double_beds: z.coerce.number().min(0, "Quantidade inválida"),
+    min_nights: z.coerce.number().min(0, "Quantidade inválida"),
+    price: z.coerce.number().min(0, "Preço Inválido"),
     amenities: z.array(z.string())
 });
 
 export const bookingSchema = z.object({
     uuid: z.string().uuid("Not UUID").optional(),
-    created_at: z.string().datetime("Data Inválida"),
-    status: z.string().trim().min(1, "Você deve preencher esse campo").optional(),
-    checkIn: z.string().datetime("Data Inválida"),
-    checkOut: z.string().datetime("Data Inválida"),
+    created_at: z.string().optional(),
+    status: z.string().optional(),
+    checkIn: z.string(),
+    checkOut: z.string(),
     guest: guestSchema,
     accommodation: accommodationSchema
 })
