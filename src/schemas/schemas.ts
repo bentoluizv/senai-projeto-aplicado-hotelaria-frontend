1a255afc829662cf151b8acd6025e5db320ee18b
Bento Luiz <bentoluizv@gmail.com>
2024-06-27 12:32:51 -0300

    add data schemas

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,38 +1,70 @@
 import { z } from "zod";
 
 export const guestSchema = z.object({
-    created_at: z.string().optional(),
     document: z.string(),
-    // document: z.string().refine((value) => isValidCpf(value), "Cpf Inválido"),
-    name: z.string().trim().min(1, "Você deve preencher esse campo"),
-    surname: z.string().trim().min(1, "Você deve preencher esse campo"),
-    country: z.string().trim().min(1, "Você deve preencher esse campo"),
-    phone: z.string().trim().min(1, "Você deve preencher esse campo"),
+    created_at: z.string(),
+    name: z.string(),
+    surname: z.string(),
+    country: z.string(),
+    phone: z.string(),
+});
+
+export const creationalGuestSchema =
+z.object({
+    document: z.string(),
+    name: z.string(),
+    surname: z.string(),
+    country: z.string(),
+    phone: z.string(),
 });
 
 export const accommodationSchema = z.object({
-    id : z.coerce.number().optional(),
-    created_at: z.string().optional(),
-    name: z.string().trim().min(1, "Você deve preencher esse campo"),
-    status: z.string().optional(),
-    total_guests: z.coerce.number().min(1, "Total de hóspedes não pode  ser menor que um"),
-    single_beds: z.coerce.number().min(0, "Quantidade inválida"),
-    double_beds: z.coerce.number().min(0, "Quantidade inválida"),
-    min_nights: z.coerce.number().min(0, "Quantidade inválida"),
-    price: z.coerce.number().min(0, "Preço Inválido"),
-    amenities: z.array(z.string())
+    id : z.coerce.number(),
+    created_at: z.string(),
+    name: z.string().trim(),
+    status: z.string(),
+    total_guests: z.coerce.number().min(1),
+    single_beds: z.coerce.number().min(0),
+    double_beds: z.coerce.number().min(0),
+    min_nights: z.coerce.number().min(0),
+    price: z.coerce.number().min(0),
+    amenities: z.array(z.object({
+        id: z.string(),
+        name: z.string()
+    }))
 });
 
+export const creationalAccommodationSchema = z.object({
+    name: z.string(),
+    status: z.string(),
+    total_guests: z.coerce.number().min(1),
+    single_beds: z.coerce.number().min(0),
+    double_beds: z.coerce.number().min(0),
+    min_nights: z.coerce.number().min(0),
+    price: z.coerce.number().min(0),
+    amenities: z.array(z.object({
+        id: z.string(),
+        name: z.string()
+    }))
+});
+
+
 export const bookingSchema = z.object({
-    uuid: z.string().uuid("Not UUID").optional(),
-    createdAt: z.string().optional(),
     status: z.string().optional(),
     checkIn: z.string(),
     checkOut: z.string(),
-    guestName: z.string(),
-    guestPhone: z.string(),
-    accommodationName: z.string(),
-    accommodationPrice: z.string(),
+    guest: guestSchema,
+    accommodation: accommodationSchema,
     budget: z.string(),
-    totalNights: z.string()
-})
+});
+
+export const creationalBookingSchema = z.object({
+    uuid: z.string().uuid(),
+    createdAt: z.string().datetime(),
+    status: z.string().optional(),
+    check_in: z.string().datetime(),
+    check_out: z.string().datetime(),
+    guest_document: z.string(),
+    accommodation_id: z.coerce.number(),
+    budget: z.string(),
+});
