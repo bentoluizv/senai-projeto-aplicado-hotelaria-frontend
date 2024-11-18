0ddf2fa8bd7a6910f27e679b34a27a991fca76fb
bentoluizv <bentoluizv@gmail.com>
2024-11-18 10:59:24 -0300

    add current use interface to astro locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface User {
+  id: string;
+  username: string;
+  role?: "USER" | "ADMIN";
+}
+
+declare namespace App {
+  interface Locals {
+    user?: User;
+  }
+}
