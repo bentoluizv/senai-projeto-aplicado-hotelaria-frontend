458301980127a201ffa84ce60e3b411e98c3e1b2
bentoluizv <bentoluizv@gmail.com>
2024-11-20 22:37:10 -0300

    add actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,21 +1,9 @@
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
-import { getRefreshedToken } from "../utils/refreshToken";
-import { setSecureToken } from "../utils/setSecureToken";
-
-const token = z.object({
-  access_token: z.string(),
-  token_type: z.string(),
-});
-
-const refreshToken = defineAction({
-  input: token,
-  handler: async (input, { cookies }) => {
-    const refreshToken = await getRefreshedToken(input);
-    setSecureToken(cookies, refreshToken);
-  },
-});
+import { getCurrentUserAction } from "./getCurrentUserAction";
+import { loginAction } from "./loginAction";
+import { refreshTokenAction } from "./refreshTokenAction";
 
 export const server = {
-  refreshToken,
+  refreshTokenAction,
+  getCurrentUserAction,
+  loginAction,
 };
