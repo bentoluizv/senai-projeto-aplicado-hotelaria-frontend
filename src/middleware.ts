9fbd45da49ff18e1f5d3a2784dde7550fc45296a
bentoluizv <bentoluizv@gmail.com>
2024-11-20 22:26:02 -0300

    refact middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
 import { actions } from "astro:actions";
-import { defineMiddleware } from "astro:middleware";
+import { defineMiddleware, sequence } from "astro:middleware";
 
-export const onRequest = defineMiddleware(async (context, next) => {
+const protectedPaths = [
+  "/",
+  "/hospedes",
+  "/hospedes/cadastro",
+  "/acomodacoes",
+  "/acomodacoes/cadastro",
+  "/reservas",
+  "/reservas/cadastro",
+];
+
+const checkTokenMiddleware = defineMiddleware(async (context, next) => {
   const { redirect, cookies, url, callAction } = context;
-
-  const protectedPaths = [
-    "/",
-    "/hospedes",
-    "/hospedes/cadastro",
-    "/acomodacoes",
-    "/acomodacoes/cadastro",
-    "/reservas",
-    "/reservas/cadastro",
-  ];
 
   if (protectedPaths.includes(url.pathname)) {
     const tokenCookie = cookies.get("token");
@@ -22,7 +22,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     }
 
     const token: Token = tokenCookie.json();
-    const { _, error } = await callAction(actions.refreshToken, token);
+    const { data, error } = await callAction(actions.refreshTokenAction, token);
 
     if (error) {
       return redirect("/login");
@@ -31,3 +31,31 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   return next();
 });
+
+const getUserMiddleware = defineMiddleware(async (context, next) => {
+  const { redirect, cookies, url, callAction, locals } = context;
+
+  if (protectedPaths.includes(url.pathname)) {
+    const tokenCookie = cookies.get("token");
+
+    if (!tokenCookie) {
+      return redirect("/login");
+    }
+
+    const token: Token = tokenCookie.json();
+    const { data, error } = await callAction(
+      actions.getCurrentUserAction,
+      token
+    );
+
+    if (error) {
+      return redirect("/login");
+    }
+
+    // locals.user = data;
+  }
+
+  return next();
+});
+
+export const onRequest = sequence(checkTokenMiddleware, getUserMiddleware);
