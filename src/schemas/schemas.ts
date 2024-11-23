ea803eadd88544324f9a4308774a1c71aff346ce
Bento Machado <bentoluizvgmail.com>
2024-11-23 16:21:30 -0300

    fix user schema

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -102,7 +102,7 @@ export const creationalBookingSchema = z.object({
 export const userSchema = z.object({
   ulid: z.string().ulid(),
   email: z.string().email(),
-  role: z.enum(["admin", "role"]),
+  role: z.enum(["admin", "user"]),
 });
 
 export type User = z.infer<typeof userSchema>;
