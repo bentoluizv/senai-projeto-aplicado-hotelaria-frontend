8312e022491dd2b8588cdf9ca2fae83ef46c4cbe
Bento Machado <bentoluizvgmail.com>
2024-11-23 16:08:52 -0300

    add update guest util func

---
+++ src/utils/updateGuest.ts
@@ -0,0 +1,28 @@
+import { type UpdateGuestDTO } from "../schemas/schemas";
+
+export const updateGuest = async (token: Token, updateData: UpdateGuestDTO) => {
+  const response = await fetch(
+    `http://backend:8050/guests/${updateData.ulid}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token.token_type} ${token.access_token}`,
+      },
+      body: JSON.stringify(updateData),
+    }
+  );
+
+  if (response.status == 401) {
+    throw new Error(`Auth failed`);
+  }
+
+  if (response.status != 200) {
+    throw new Error(
+      `Something went wrong with your request!! Response status: ${response.status}`
+    );
+  }
+
+  const data: { message: string } = await response.json();
+  return data;
+};
