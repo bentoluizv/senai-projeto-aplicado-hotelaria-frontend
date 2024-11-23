d4fa3013093abca2f3fc8d2ca9b4e89bd02e3cd0
Bento Machado <bentoluizvgmail.com>
2024-11-23 18:41:58 -0300

    add amenitie schema

--- src/schemas/accommodation.ts
+++ src/schemas/accommodation.ts
@@ -1,5 +1,14 @@
 import { z } from "astro:content";
 
+export const amenittieSchema = z.object({
+  id: z.coerce.string().trim(),
+  name: z.coerce.string().trim(),
+});
+
+export const createAmenitieDTO = z.object({
+  name: z.coerce.string().trim(),
+});
+
 export const accommodationSchema = z.object({
   ulid: z.string().ulid(),
   name: z.coerce.string().trim(),
@@ -8,12 +17,7 @@ export const accommodationSchema = z.object({
   single_beds: z.coerce.number().min(0),
   double_beds: z.coerce.number().min(0),
   price: z.coerce.number().min(0),
-  amenities: z.array(
-    z.object({
-      id: z.coerce.string().trim(),
-      name: z.coerce.string().trim(),
-    })
-  ),
+  amenities: z.array(amenittieSchema),
 });
 
 export const updateAccommodationSchema = z.object({
@@ -23,7 +27,7 @@ export const updateAccommodationSchema = z.object({
   single_beds: z.coerce.number().min(0).optional(),
   double_beds: z.coerce.number().min(0).optional(),
   price: z.coerce.number().min(0).optional(),
-  amenities: z.array(z.string()).optional(),
+  amenities: z.array(amenittieSchema).optional(),
 });
 
 export const creationalAccommodationSchema = z.object({
@@ -32,12 +36,13 @@ export const creationalAccommodationSchema = z.object({
   single_beds: z.coerce.number().min(0),
   double_beds: z.coerce.number().min(0),
   price: z.coerce.number().min(0),
-  amenities: z.array(z.string()),
+  amenities: z.array(amenittieSchema),
 });
 
 export type Accommodation = z.infer<typeof accommodationSchema>;
+export type Amenitie = z.infer<typeof amenittieSchema>;
 export type UpdateAccommodationDTO = z.infer<typeof updateAccommodationSchema>;
-
+export type CreateAmenitieDTO = z.infer<typeof createAmenitieDTO>;
 export type CreateAccommodationDTO = z.infer<
   typeof creationalAccommodationSchema
 >;
