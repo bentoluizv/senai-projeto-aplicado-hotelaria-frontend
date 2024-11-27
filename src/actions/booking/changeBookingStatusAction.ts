fd6af09d4f8ba9964139eed70f15b564cf8b11db
Bento Machado <bentoluizvgmail.com>
2024-11-27 00:41:42 -0300

    change action logic

--- src/actions/booking/changeBookingStatusAction.ts
+++ src/actions/booking/changeBookingStatusAction.ts
@@ -4,12 +4,12 @@ import { getToken } from "../../utils/auth/getToken";
 import { changeBookingStatus } from "../../utils/booking/changeBookingStatus";
 
 export const changeBookingStatusAction = defineAction({
-  accept: "form",
   input: updateBookingSchema,
   handler: async (input, ctx) => {
     const { cookies } = ctx;
+    const { status, ulid } = input;
     const token = await getToken(cookies);
-    const data: { message: string } = await changeBookingStatus(token, input);
+    const data = await changeBookingStatus(token, ulid, status);
     return data;
   },
 });
