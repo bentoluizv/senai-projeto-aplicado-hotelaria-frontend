f66fefd8b092fdc72adfb66e9c75a769f8b2454e
Bento Machado <bentoluizvgmail.com>
2024-11-27 21:29:28 -0300

    remove locals for token

--- src/actions/auth/loginAction.ts
+++ src/actions/auth/loginAction.ts
@@ -6,11 +6,13 @@ import { getLoginFormData } from "../../utils/getLoginFormData";
 const loginAction = defineAction({
   accept: "form",
   handler: async (input, ctx) => {
-    const { cookies, locals } = ctx;
+    const { cookies } = ctx;
+
     const data = await getLoginFormData(input);
+
     const jwt = await loginForAccessToken(data, cookies);
+
     setSecureToken(cookies, jwt);
-    locals.token = jwt;
   },
 });
 
